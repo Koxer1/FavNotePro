cb3ff468532018e340fa91b49e1f7fb415144644
Konrad Lomzik <koxer101@gmail.com>
2021-01-12 13:07:29 +0100

    added styled-components button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from './views/Root/Root';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-
-  document.getElementById('root'),
-);
+ReactDOM.render(<Root />, document.getElementById('root'));
