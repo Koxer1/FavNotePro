3590e9fbec2a001d68b77dd0cedee2206c513c1e
Konrad Lomzik <koxer101@gmail.com>
2021-01-15 18:45:59 +0100

    Added Routs, created views

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './views/Root/Root';
+import Root from './views/Root';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
